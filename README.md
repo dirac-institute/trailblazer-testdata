d3b3de57baa7a700136f3e20f68ba502210729df
DinoBektesevic <dinob@uw.edu>
2021-09-07 13:52:03 -0700

    Create test data repo for trailblazer.

---
+++ README.md
@@ -0,0 +1,16 @@
+# trailblazer-testdata
+
+``trailblazer-testdata`` provides a sample input and output data sufficient to test the functionality of ``trailblazer``.
+
+This repository provides the configuration files required by ``trailblazer`` to use the data.
+
+Thumbnails displayed in the ``trailblazer`` gallery can be found in the ``media/`` directory. 
+The data that was used to produce the thumbnails is located in the ``static/upload/fits`` directory. The collection of FITS files that can be found there have been resized in order to keep the total size of this repository reasonable, while being sufficient in their role as placeholder files for download or any other ``trailblazer`` functionality that depends on their existence. The raw, original, unresized data is provided in the same directory as a ``.tar.bz2`` archive. If untampered data is required for a particular purpose, extract the archive in the same directory.
+
+A database, also required for ``trailblazer`` to work correctly, is provided in the root directory of the repository as ``db.sqlite3``. 
+
+There are no guarantees the current iteration of the thumbnails, test data or the database matches the exact state of the [trailblazer](https://github.com/dirac-institute/trailblazer) repository main branch, even though we are trying to be as up to date as possible. Open an Issue here or in the [trailblazer](https://github.com/dirac-institute/trailblazer) repository if the current version is too far out of sync with the main repo.
+
+## Obtaining the test data
+
+The data is stored using [Git LFS](https://git-lfs.github.com/).
---
+++ scripts/export_reduced.py
@@ -0,0 +1,125 @@
+"""
+Utilities used to crop, update the center and dimensions of fits files in order
+to reduce their size.
+
+Utility will, by default, replace files in-place.
+"""
+
+
+import os
+import argparse
+
+import astropy.io.fits as fits
+
+
+def crop_and_shift(hdu, centerpix, crop=200):
+    endSize = int(crop*2)
+    cr1, cr2 = centerpix
+    hdu.data = hdu.data[cr1-crop:cr1+crop, cr2-crop:cr2+crop]
+    hdu.header["NAXIS1"] = endSize
+    hdu.header["NAXIS2"] = endSize
+    return hdu
+
+
+def reduce_moa(hdulist):
+    primary = hdulist[0]
+    cr1, cr2 = int(primary.header["NAXIS1"]/2), int(primary.header["NAXIS2"]/2)
+    return crop_and_shift(primary, (cr1, cr2))
+
+
+def reduce_lcogt(hdulist):
+    primary = hdulist[0]
+    cr1, cr2 = primary.header["CRPIX1"], primary.header["CRPIX2"]
+    return crop_and_shift(primary, (cr1, cr2))
+
+
+def reduce_sdss(hdulist):
+    primary = hdulist[0]
+    cr1, cr2 = int(primary.header["CRPIX1"]), int(primary.header["CRPIX2"])
+    return crop_and_shift(primary, (cr1, cr2))
+
+
+def reduce_rubin_decam(hdulist):
+    # we have an old version of a file, but it's the only one with lines in it
+    # and this is a problem because we can't actually do it with vanilla astropy
+    #    https://github.com/astropy/astropy/issues/10554
+    # # Fix requires editing io/fits/hdu/compressed.py
+    # l. 1666 and removing ZQUANTIZ flag from header by, for example:
+    #     if self._header.get("ZQUANTIZ", False) == "NONE":
+    #         del self._header["ZQUANTIZ"]
+    # otherwise it will fail with an RuntimeError in the compression C code
+    for i in range(1, 4):
+        cr1, cr2 = int(hdulist[i].header["CRPIX1"]), int(hdulist[i].header["CRPIX2"])
+        hdulist[i] = crop_and_shift(hdulist[i], (cr1, cr2))
+
+    return hdulist
+
+
+def reduce_hsc(hdulist):
+    primary = hdulist[0]
+    primary.data = primary.data[1700:2100, 0:400]
+    primary.header["CRPIX2"] -= 1700
+    primary.header["NAXIS1"] = 400
+    primary.header["NAXIS2"] = 400
+
+    return hdulist
+
+
+def reduce_community_decam(hdulist):
+    for ext in hdulist[1:]:
+        # crpix in deca
+        # cr1, cr2 = int(ext.header["CRPIX1"]), int(ext.header["CRPIX2"])
+        cr1, cr2 = int(ext.header["NAXIS1"]/2), int(ext.header["NAXIS2"]/2)
+        ext.header["CRPIX1"] = ext.header["CRPIX1"] - (cr1-50)
+        ext.header["CRPIX2"] = ext.header["CRPIX2"] - (cr2-50)
+        # for some reason we are not allowed to touch NAXIS here
+        # ext.header["NAXIS1"] = 400
+        # ext.header["NAXIS2"] = 400
+        ext.data = ext.data[cr1-50:cr1+50, cr2-50:cr2+50]
+
+    return hdulist
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Utility for cropping and reframing of fits files.")
+    parser.add_argument("-i", "--input", nargs="?",  default="../static/upload/fits/",
+                        help=("Directory containing unresized fits files. Default: ../static/upload/fits/"))
+    parser.add_argument("-o", "--output", nargs="?",  default="../static/upload/fits/",
+                        help=("Output directory. Default: ../static/upload/fits/"))
+    args = parser.parse_args()
+
+    if os.path.exists(args.input) and os.path.isdir(args.input):
+        datadir = os.path.abspath(args.input)
+    else:
+        raise ValueError("Input location is not a directory or doesn't exist!")
+
+    outdir = os.path.abspath(args.output)
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
+
+    files = os.listdir(datadir)
+    for f in files:
+        if "fits" in f:
+            try:
+                fullpath = os.path.join(datadir, f)
+                hdulist = fits.open(fullpath)
+                if f.startswith("frame"):
+                    hdulist = reduce_sdss(hdulist)
+                elif f.startswith("bi"):
+                    hdulist = reduce_lcogt(hdulist)
+                elif ("obstel" in hdulist[0].header) and \
+                     ("moa" in hdulist[0].header["OBSTEL"].lower()):
+                    hdulist = reduce_moa(hdulist)
+                elif f.startswith("calexp"): # this might need updating later
+                    hdulist = reduce_rubin_decam(hdulist)
+                elif f.startswith("HSC"):
+                    hdulist = reduce_hsc(hdulist)
+                elif f.startswith("c4d"):
+                    hdulist = reduce_community_decam(hdulist)
+                else:
+                    pass
+                hdulist.writeto(os.path.join(outdir, f), overwrite=True)
+            except Exception as e:
+                print(f"Could not process {f}:\n{e}\n\n")
